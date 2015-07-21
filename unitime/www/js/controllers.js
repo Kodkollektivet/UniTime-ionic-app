8886c228b8425f1307eaed6f96af6d64278cef04
OttoBergman <otto_bergman@live.se>
2015-07-21 23:27:57 +0200

    Added master controller

--- unitime/www/js/controllers.js
+++ unitime/www/js/controllers.js
@@ -1,8 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats, Course) {
+.controller('app', function($scope){
+      $scope.data;
+})
+
+.controller('app.DashCtrl', function($scope) {})
+
+.controller('app.ChatsCtrl', function($scope, Chats, Course) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),

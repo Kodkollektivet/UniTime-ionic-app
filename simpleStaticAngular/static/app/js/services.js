4d91fe4b9eb1817fc6bd57a2855476164d004835
John Herrlin <jherrlin@gmail.com>
2015-10-06 17:58:41 +0200

    Cleanup made, old projects removed from repo.

--- simpleStaticAngular/static/app/js/services.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp.services', ['ngResource'])
-
-    .factory('Course', function($resource){
-        return $resource('http://unitime.se/api/course/', {}, {
-            query: {method:"GET", isArray:true}
-        });
-    });
--- simpleStaticAngularUiRouterResolve/static/js/app/controllers.js
+++
@@ -1,8 +0,0 @@
-
-angular.module('controllers', [])
-
-    .controller('EventController', ['$scope', 'item', function($scope, item){
-        $scope.items = item;  // This is the object that is comming from the resolve
-        $scope.hejsan = 'Hejsan';  // This is a static scope variable
-        console.log(item);
-    }]);
